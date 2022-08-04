9dcf1bd6d05988e3864759d52dbce2d2560a023f
423 baba <manthan.nadcab@gmail.com>
2022-08-04 17:16:13 +0530

    tile and block correction

--- apps/block_scout_web/assets/js/lib/from_now.js
+++ apps/block_scout_web/assets/js/lib/from_now.js
@@ -20,15 +20,20 @@ function tryUpdateAge (el) {
 }
 function updateAge (el, timestamp) {
   let fromNow = timestamp.fromNow()
+  // console.log("el::",el,"fromNow ::",fromNow);
+  let timechunck = fromNow.split(" ");
   // show the exact time only for transaction details page. Otherwise, short entry
-  const elInTile = el.hasAttribute('in-tile')
+  const elInTile = el.hasAttribute('in-tile');
+  const timeBlock = el.hasAttribute('time-block'); 
   if ((window.location.pathname.includes('/tx/') || window.location.pathname.includes('/block/') || window.location.pathname.includes('/blocks/')) && !elInTile) {
     const offset = moment().utcOffset() / 60
     const sign = offset && Math.sign(offset) ? '+' : '-'
     const formatDate = `MMMM-DD-YYYY hh:mm:ss A ${sign}${offset} UTC`
     fromNow = `${fromNow} | ${timestamp.format(formatDate)}`
+      // console.log("element::",el,"from_now::",fromNow);
   }
-  if (fromNow !== el.innerHTML) el.innerHTML = fromNow
+  if (fromNow !== el.innerHTML && !timeBlock) el.innerHTML = fromNow;
+  if (timeBlock) el.innerHTML =`<span class="digi_time">${timechunck[0]} </span> <span class="digi_time">${timechunck[1]} </span> <span class="digi_time">${timechunck[2]}</span>`;
 }
 updateAllAges()
 
